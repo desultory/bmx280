992c307f719c3986823d8bb7711d03e1ad060c8b
Zen <z@pyl.onl>
2024-01-29 12:08:57 -0600

    add switch for device check, update image
    Signed-off-by: Zen <z@pyl.onl>

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from utime import sleep_ms
 from machine import Pin, freq
 
 led = Pin(25, Pin.OUT)
+missing_toggle = Pin(16, Pin.IN)
+missing_pwr = Pin(17, Pin.OUT)
+missing_pwr.value(1)
 
 i2c_controllers = {0: {'pins': (0, 1)},
                    1: {'pins': (26, 27)}}
 
+ignore_missing = bool(missing_toggle.value())
+
 try:
-    s = SensorController(i2c_controllers, ignore_missing=False, interval=250)
+    s = SensorController(i2c_controllers, ignore_missing=ignore_missing, interval=250)
 except Exception as e:
     print(e)
     for _ in range(50):
